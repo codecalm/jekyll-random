d9125d666e9bb515d9fee457ba39d297f508f029
chomik <codecalm@gmail.com>
2018-03-07 16:13:29 +0100

    first version of plugin

--- random.rb
+++ random.rb
@@ -1,3 +1,14 @@
 module Jekyll
+   module RandomFilter
 
+      def random_number(value, max=100, min=0, offset=0, round=0)
+         formatter = round ? '%.' + round.to_s + 'f' : '%'
+         value = formatter % ((Math.sin(Math.sin(value + offset)) + 1) * ((max - min) / 2) + min)
+
+         value
+      end
+
+   end
 end
+
+Liquid::Template.register_filter(Jekyll::RandomFilter)
